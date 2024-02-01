33232cdbd12dc3ac2c7fa7c89c35acfb1a08a001
nishiinoya <omalanchuk.wrk@gmail.com>
2024-02-01 01:55:12 +0100

    shorter func lines

--- race00/mx_cube.c
+++ race00/mx_cube.c
@@ -1,8 +1,10 @@
 void mx_printchar(char c);
 void horizontal_line(int HORIZONTAL_LENGTH);
-void upper_side(int DIAGONAL_LENGTH, int HORIZONTAL_LENGTH);
+void upper_side(int DIAGONAL_LENGTH,
+                int HORIZONTAL_LENGTH);
 void print_front(int HORIZONTAL_LENGTH);
-void lower_side(int row, int HORIZONTAL_LENGTH, int VERTICAL_LENGTH);
+void lower_side(int row, int HORIZONTAL_LENGTH,
+                int VERTICAL_LENGTH);
 
 void mx_cube(int n) {
     const int SHIFT_UPPER_BACK = n / 2 + 1;
@@ -20,7 +22,7 @@ void mx_cube(int n) {
     horizontal_line(HORIZONTAL_LENGTH);
     mx_printchar('\n');
     upper_side(DIAGONAL_LENGTH, HORIZONTAL_LENGTH);
-    horizontal_line(HORIZONTAL_LENGTH); // UPPER_FRONT HORIZONTAL
+    horizontal_line(HORIZONTAL_LENGTH);
     for (int j = 0; j < DIAGONAL_LENGTH; j++) {
         mx_printchar(' ');
     }
@@ -30,7 +32,8 @@ void mx_cube(int n) {
     for (int row = 0; row < VERTICAL_LENGTH; row++) {
         print_front(HORIZONTAL_LENGTH);
 
-        int back_corner_idx = VERTICAL_LENGTH - DIAGONAL_LENGTH - 1;
+        int back_corner_idx = VERTICAL_LENGTH -
+                              DIAGONAL_LENGTH - 1;
 
         if (row < back_corner_idx) {
             for (int j = 0; j < DIAGONAL_LENGTH; j++) {
@@ -43,7 +46,8 @@ void mx_cube(int n) {
             }
             mx_printchar('+');
         } else {
-            lower_side(row, HORIZONTAL_LENGTH, VERTICAL_LENGTH);
+            lower_side(row, 
+            HORIZONTAL_LENGTH, VERTICAL_LENGTH);
         }
         mx_printchar('\n');
     }
@@ -53,7 +57,6 @@ void mx_cube(int n) {
 
     return;
 }
-
 
 void horizontal_line(int HORIZONTAL_LENGTH) {
     mx_printchar('+');
@@ -71,15 +74,17 @@ void print_front(int HORIZONTAL_LENGTH) {
     mx_printchar('|');
 }
 
-void lower_side(int row, int HORIZONTAL_LENGTH, int VERTICAL_LENGTH) {
-    int start_side_idx = HORIZONTAL_LENGTH + 1;
-    int end_side_idx = start_side_idx + VERTICAL_LENGTH - row - 1;
-    for (int i = start_side_idx; i < end_side_idx; i++) {
+void lower_side(int row,
+                int HORIZONTAL_LENGTH, int VERTICAL_LENGTH) {
+    int start_idx = HORIZONTAL_LENGTH + 1;
+    int end_idx = start_idx + VERTICAL_LENGTH - row - 1;
+    for (int i = start_idx; i < end_idx; i++) {
         mx_printchar(' ');
     }
     mx_printchar('/');
 }
-void upper_side(int DIAGONAL_LENGTH, int HORIZONTAL_LENGTH) {
+void upper_side(int DIAGONAL_LENGTH,
+                int HORIZONTAL_LENGTH) {
     for (int i = 0; i < DIAGONAL_LENGTH; i++) {
         for (int j = 0; j < DIAGONAL_LENGTH - i; j++) {
             mx_printchar(' ');
