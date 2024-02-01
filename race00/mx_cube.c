b060267ba95c18e3c6c75496c887876798df818e
nishiinoya <omalanchuk.wrk@gmail.com>
2024-02-01 15:32:40 +0100

    lgth -> len

--- race00/mx_cube.c
+++ race00/mx_cube.c
@@ -1,42 +1,42 @@
 void mx_printchar(char c);
-void horizontal_line(int horz_lgth);
-void upper_side(int diag_lgth, int horz_lgth);
-void corner(int vert_lgth, int horz_lgth, int diag_lgth);
-void print_spaces(int lgth);
+void horizontal_line(int horz_len);
+void upper_side(int diag_len, int horz_len);
+void corner(int vert_len, int horz_len, int diag_len);
+void print_spaces(int len);
 
 void mx_cube(int n) {
-    const int horz_lgth = 2 * n;
-    const int diag_lgth = n / 2;
-    const int vert_lgth = n;
+    const int horz_len = 2 * n;
+    const int diag_len = n / 2;
+    const int vert_len = n;
     if (n <= 1)
         return;
     print_spaces(n / 2 + 1);
-    horizontal_line(horz_lgth);
+    horizontal_line(horz_len);
     mx_printchar('\n');
-    upper_side(diag_lgth, horz_lgth);
-    horizontal_line(horz_lgth);
-    for (int j = 0; j < diag_lgth; j++) {
+    upper_side(diag_len, horz_len);
+    horizontal_line(horz_len);
+    for (int j = 0; j < diag_len; j++) {
         mx_printchar(' ');
     }
     mx_printchar('|');
     mx_printchar('\n');
-    corner(vert_lgth, horz_lgth, diag_lgth);
-    horizontal_line(horz_lgth);
+    corner(vert_len, horz_len, diag_len);
+    horizontal_line(horz_len);
     mx_printchar('\n');
 }
 
-void horizontal_line(int horz_lgth) {
+void horizontal_line(int horz_len) {
     mx_printchar('+');
-    for (int i = 0; i < horz_lgth; i++) {
+    for (int i = 0; i < horz_len; i++) {
         mx_printchar('-');
     }
     mx_printchar('+');
 }
-void upper_side(int diag_lgth, int horz_lgth) {
-    for (int i = 0; i < diag_lgth; i++) {
-        print_spaces(diag_lgth - i);
+void upper_side(int diag_len, int horz_len) {
+    for (int i = 0; i < diag_len; i++) {
+        print_spaces(diag_len - i);
         mx_printchar('/');
-        print_spaces(horz_lgth);
+        print_spaces(horz_len);
         mx_printchar('/');
         print_spaces(i);
         mx_printchar('|');
@@ -44,21 +44,21 @@ void upper_side(int diag_lgth, int horz_lgth) {
     }
 }
 
-void corner(int vert_lgth, int horz_lgth, int diag_lgth) {
-    for (int row = 0; row < vert_lgth; row++) {
+void corner(int vert_len, int horz_len, int diag_len) {
+    for (int row = 0; row < vert_len; row++) {
         mx_printchar('|');
-        print_spaces(horz_lgth);
+        print_spaces(horz_len);
         mx_printchar('|');
-        int corner_i = vert_lgth - diag_lgth - 1;
+        int corner_i = vert_len - diag_len - 1;
         if (row < corner_i) {
-            print_spaces(diag_lgth);
+            print_spaces(diag_len);
             mx_printchar('|');
         } else if (row == corner_i) {
-            print_spaces(diag_lgth);
+            print_spaces(diag_len);
             mx_printchar('+');
         } else {
-            int start_i = horz_lgth + 1;
-            int end_i = start_i + vert_lgth - row - 1;
+            int start_i = horz_len + 1;
+            int end_i = start_i + vert_len - row - 1;
             for (int i = start_i; i < end_i; i++) {
                 mx_printchar(' ');
             }
@@ -68,8 +68,8 @@ void corner(int vert_lgth, int horz_lgth, int diag_lgth) {
     }
 }
 
-void print_spaces(int lgth) {
-    for (int j = 0; j < lgth; j++) {
+void print_spaces(int len) {
+    for (int j = 0; j < len; j++) {
         mx_printchar(' ');
     }
 }
